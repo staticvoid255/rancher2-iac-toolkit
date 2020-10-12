d09dc7e3cfdba6bdc587682d80b3b74983fbae03
Static Void <staticvoidnetwork@outlook.com>
2020-10-12 11:14:28 +0200

    basic provider config

---
+++ providers.tf
@@ -0,0 +1,24 @@
+terraform {
+	required_version = ">= 0.13"
+
+	required_providers {
+		rancher2 = {
+			source = "rancher/rancher2"
+			version = "1.10.1"
+		}
+	}
+}
+
+provider "rancher2" {
+    alias = "bootstrap"
+    api_url   = local.rancher_api_url
+    bootstrap = true
+}
+
+provider "rancher2" {
+	alias = "admin"
+
+	api_url = rancher2_bootstrap.admin.url
+	token_key = rancher2_bootstrap.admin.token
+	insecure = true
+}
